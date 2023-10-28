cf6be52d54e781a18f21ddd1992d2efdba090c07
giovannasilva1 <128196391+giovannasilva1@users.noreply.github.com>
2023-10-27 21:23:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,16 +11,24 @@ document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador=0;
-while(contador <listaDeTeclas.length) {
+for(let contador=0; contador <listaDeTeclas.length; contador++) {
   const tecla= listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio =`#som_${instrumento}`;
-  tecla.onclick =function () {
+  tecla.onclick =function () {  
   tocaSom(idAudio);
 };
-  contador = contador + 1;
-  console.log(contador);
+
+  tecla.onkeydown =function (evento) {
+  if (evento.code === 'Enter') {
+  tecla.classList.add('ativa');
+  }
+  console.log(evento.code);
+}
+  
+  tecla.onkeyup =function () {
+  tecla.classList.remove('ativa');
+}
+  
   console.log(instrumento);
-  console.log(idAudio);
 }
